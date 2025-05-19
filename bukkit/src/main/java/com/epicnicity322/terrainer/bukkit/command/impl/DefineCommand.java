5ca8fb7293f4d0ad333fd424e3167e79bfdb0e42
chrisrnj <christianorangel322@gmail.com>
2025-05-18 23:56:54 -0300

    Made define command's terrain flags configurable

--- bukkit/src/main/java/com/epicnicity322/terrainer/bukkit/command/impl/DefineCommand.java
+++ bukkit/src/main/java/com/epicnicity322/terrainer/bukkit/command/impl/DefineCommand.java
@@ -1,6 +1,6 @@
 /*
  * Terrainer - A minecraft terrain claiming protection plugin.
- * Copyright (C) 2024 Christiano Rangel
+ * Copyright (C) 2025 Christiano Rangel
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -16,35 +16,46 @@
  * along with this program.  If not, see <https://www.gnu.org/licenses/>.
  */
 
-package com.epicnicity322.terrainer.bukkit.command;
+package com.epicnicity322.terrainer.bukkit.command.impl;
 
-import com.epicnicity322.epicpluginlib.bukkit.command.Command;
 import com.epicnicity322.epicpluginlib.bukkit.command.CommandRunnable;
 import com.epicnicity322.epicpluginlib.bukkit.lang.MessageSender;
+import com.epicnicity322.epicpluginlib.core.logger.ConsoleLogger;
 import com.epicnicity322.terrainer.bukkit.TerrainerPlugin;
+import com.epicnicity322.terrainer.bukkit.command.TerrainerCommand;
 import com.epicnicity322.terrainer.bukkit.event.terrain.UserCreateTerrainEvent;
 import com.epicnicity322.terrainer.bukkit.event.terrain.UserNameTerrainEvent;
 import com.epicnicity322.terrainer.bukkit.util.CommandUtil;
+import com.epicnicity322.terrainer.core.Terrainer;
+import com.epicnicity322.terrainer.core.config.Configurations;
 import com.epicnicity322.terrainer.core.event.terrain.IUserNameTerrainEvent;
+import com.epicnicity322.terrainer.core.flag.Flag;
+import com.epicnicity322.terrainer.core.flag.FlagTransformException;
 import com.epicnicity322.terrainer.core.flag.Flags;
 import com.epicnicity322.terrainer.core.location.Coordinate;
 import com.epicnicity322.terrainer.core.location.WorldCoordinate;
 import com.epicnicity322.terrainer.core.terrain.Terrain;
 import com.epicnicity322.terrainer.core.terrain.TerrainManager;
 import com.epicnicity322.terrainer.core.util.PlayerUtil;
+import com.epicnicity322.yamlhandler.Configuration;
+import com.epicnicity322.yamlhandler.ConfigurationSection;
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 import org.jetbrains.annotations.NotNull;
 
+import java.util.HashSet;
+import java.util.Map;
 import java.util.UUID;
 
 /**
  * A command designed to create terrains that are protected against any threat.
  * This is useful for server buildings, such as the spawn.
  */
-public final class DefineCommand extends Command {
+public final class DefineCommand extends TerrainerCommand {
+    private static final @NotNull HashSet<FlagEntry<?>> defineFlags = new HashSet<>();
+
     @Override
     public @NotNull String getName() {
         return "define";
@@ -58,6 +69,36 @@ public final class DefineCommand extends Command {
     @Override
     protected @NotNull CommandRunnable getNoPermissionRunnable() {
         return CommandUtil.noPermissionRunnable();
+    }
+
+    @Override
+    public void reloadCommand() {
+        setAliases(TerrainerPlugin.getLanguage().get("Commands.Define.Command"));
+
+        defineFlags.clear();
+        Configuration flags = Configurations.FLAGS.getConfiguration();
+        for (Map.Entry<String, Object> entry : flags.getNodes().entrySet()) {
+
+            String flagId = entry.getKey();
+            Flag<?> flag = Flags.matchFlag(flagId);
+            if (flag == null || !(entry.getValue() instanceof ConfigurationSection flagSection)) continue;
+            String defineValueSerialized = flagSection.getString("Define Value").orElse(null);
+            if (defineValueSerialized == null) continue;
+            addDefineFlagEntry(flag, defineValueSerialized);
+        }
+    }
+
+    private <T> void addDefineFlagEntry(@NotNull Flag<T> flag, @NotNull String serializedValue) {
+        try {
+            defineFlags.add(new FlagEntry<>(flag, flag.transformer().apply(serializedValue)));
+        } catch (FlagTransformException e) {
+            Terrainer.logger().log("Unable to load 'Define Value' for flag '" + flag.id() + "' - " + e.getMessage(), ConsoleLogger.Level.WARN);
+        } catch (Throwable ignored) {
+        }
+    }
+
+    private <T> void putFlagInTerrain(@NotNull FlagEntry<T> flagEntry, @NotNull Terrain terrain) {
+        terrain.flags().putFlag(flagEntry.flag(), flagEntry.value());
     }
 
     @SuppressWarnings("deprecation")
@@ -95,24 +136,8 @@ public final class DefineCommand extends Command {
             if (!event.isCancelled()) terrain.setName(event.newName());
         }
 
-        // TODO: Add missing flags that should be here on define, also make this configurable.
-        // Setting spawn protection flags.
-        Terrain.FlagMap flagMap = terrain.flags();
-        flagMap.putFlag(Flags.BLOCK_FORM, false);
-        flagMap.putFlag(Flags.BLOCK_SPREAD, false);
-        flagMap.putFlag(Flags.CAULDRONS_CHANGE_LEVEL_NATURALLY, false);
-        flagMap.putFlag(Flags.EAT, false);
-        flagMap.putFlag(Flags.ENEMY_HARM, false);
-        flagMap.putFlag(Flags.EXPLOSION_DAMAGE, false);
-        flagMap.putFlag(Flags.FIRE_DAMAGE, false);
-        flagMap.putFlag(Flags.FIRE_SPREAD, false);
-        flagMap.putFlag(Flags.FLY, false);
-        flagMap.putFlag(Flags.GLIDE, false);
-        flagMap.putFlag(Flags.LEAF_DECAY, false);
-        flagMap.putFlag(Flags.MESSAGE_LOCATION, "NONE");
-        flagMap.putFlag(Flags.MOB_SPAWN, false);
-        flagMap.putFlag(Flags.SHOW_BORDERS, false);
-        flagMap.putFlag(Flags.SPAWNERS, false);
+        // Setting strong protection flags.
+        defineFlags.forEach(flagEntry -> putFlagInTerrain(flagEntry, terrain));
 
         if (TerrainManager.add(terrain)) {
             lang.send(sender, lang.get("Create.Define").replace("<terrain>", terrain.name()));
@@ -128,5 +153,8 @@ public final class DefineCommand extends Command {
         // Clearing selections.
         selection[0] = null;
         selection[1] = null;
+    }
+
+    private record FlagEntry<T>(@NotNull Flag<T> flag, @NotNull T value) {
     }
 }
